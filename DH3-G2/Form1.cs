c743e543b3f586e03afa7ce98c6669b36da4723c
Emjr31 <adonay.chacon12@gmail.com>
2021-08-29 10:51:33 -0600

    Backend: restrimciones de datos

--- DH3-G2/Form1.cs
+++ DH3-G2/Form1.cs
@@ -25,11 +25,18 @@ namespace DH3_G2
             double c = Convert.ToDouble(txtc.Text);
             double x1, x2;
             ///Proceso de calculo de X1 y X2
-            x1 = (-b+Math.Sqrt((Math.Pow(b,2)-(4*a*c)))) / (2 * a);
-            x2 = (-b - Math.Sqrt((Math.Pow(b, 2) - (4 * a * c)))) / (2 * a);
-            ///MOstrar resultados
-            txtX1.Text = x1.ToString();
-            txtX2.Text = x2.ToString();
+            try
+            {
+                x1 = (-b + Math.Sqrt((Math.Pow(b, 2) - (4 * a * c)))) / (2 * a);
+                x2 = (-b - Math.Sqrt((Math.Pow(b, 2) - (4 * a * c)))) / (2 * a);
+                ///MOstrar resultados
+                txtX1.Text = x1.ToString();
+                txtX2.Text = x2.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Revise que los datos ingresados esten correctos.. ."+ex);
+            }
 
         }
     }
